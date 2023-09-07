d80843afe052baa6e050c0c464a774b74f18990f
danilomsousa <danilomsousa@hotmail.com>
2023-09-07 15:25:41 +0100

    Add project files.

---
+++ ASPNET_BestPractices/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPNET_BestPractices.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
